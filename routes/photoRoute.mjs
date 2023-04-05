7d00242c7bda27267af31470d59cc7df6947a494
Mamed <mammadaga@morpho.az>
2023-04-05 14:10:09 +0400

    photo

--- routes/photoRoute.mjs
+++ routes/photoRoute.mjs
@@ -1,8 +1,9 @@
 import express from "express";
-import { createPhoto } from "../controllers/photoController.mjs";
+import { createPhoto, getAllPhotos, getPhotoById } from "../controllers/photoController.mjs";
 
 const router = express.Router();
 
-router.route("/").post(createPhoto);
+router.route("/").post(createPhoto).get(getAllPhotos);
+router.route("/:id").get(getPhotoById);
 
 export default router;
